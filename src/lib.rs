14e804bcde293df08ddad8820c172715b14c3b5f
Folke Behrens <folke@gmail.com>
2024-04-06 14:11:04 +0200

    Check for parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, ItemStruct};
 
 #[proc_macro_attribute]
-pub fn typed(_attr: TokenStream, input: TokenStream) -> TokenStream {
+pub fn typed(params: TokenStream, input: TokenStream) -> TokenStream {
+  if !params.is_empty() {
+    return quote! {
+      compile_error!("This macro does not (yet) accept parameters");
+    }
+    .into();
+  }
   let item = input.clone();
   let _item = parse_macro_input!(item as ItemStruct);
 
