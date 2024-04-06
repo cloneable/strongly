62b7b1ec1c0f4368bc11e7c3f34d8a3e3a39582c
Folke Behrens <folke@gmail.com>
2024-04-06 14:42:40 +0200

    Split out main func, return Result

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,26 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ItemStruct};
+use proc_macro2::{Span, TokenStream};
+use syn::{Error, ItemStruct, Result};
 
 #[proc_macro_attribute]
-pub fn typed(params: TokenStream, input: TokenStream) -> TokenStream {
-  if !params.is_empty() {
-    return quote! {
-      compile_error!("This macro does not (yet) accept parameters");
-    }
-    .into();
+pub fn typed(
+  params: proc_macro::TokenStream,
+  input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+  match typed_main(params.into(), input.into()) {
+    Ok(output) => output.into(),
+    Err(err) => err.into_compile_error().into(),
   }
-  let item = input.clone();
-  let _item = parse_macro_input!(item as ItemStruct);
+}
 
-  input
+fn typed_main(params: TokenStream, input: TokenStream) -> Result<TokenStream> {
+  if !params.is_empty() {
+    return Err(Error::new(
+      Span::call_site(),
+      String::from("This macro does not (yet) accept parameters"),
+    ));
+  }
+
+  let _item = syn::parse2::<ItemStruct>(input.clone())?;
+
+  Ok(input)
 }
